ee874896a33125483113d6798abebe780bc47f40
Abhishek Jaiswal <abhishekjaiswal124@gmail.com>
2022-12-26 21:55:11 +0530

    Implementing Loggers design

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
+    private static LogProcessor getChainOfLogs(){
+        return new InfoLogProcessor(new DebugLogProcessor(new ErrorLogProcessor(null)));
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LogProcessor logger = getChainOfLogs();
+        logger.log(LogProcessor.INFO, "This is the message.");
+        logger.log(LogProcessor.DEBUG, "This is the message.");
+        logger.log(LogProcessor.ERROR, "This is the message.");
     }
 }
